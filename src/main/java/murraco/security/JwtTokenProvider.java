055515b8173e3f236f79e8809917c02d979b5527
ctlove0523 <478309639@qq.com>
2019-06-04 13:07:30 +0800

    Update JwtTokenProvider.java

--- src/main/java/murraco/security/JwtTokenProvider.java
+++ src/main/java/murraco/security/JwtTokenProvider.java
@@ -1,55 +1,47 @@
 package murraco.security;
 
-import java.util.Base64;
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
-
-import javax.annotation.PostConstruct;
-import javax.servlet.http.HttpServletRequest;
-
+import io.github.ctlove0523.jwt.model.Role;
+import io.github.ctlove0523.jwt.service.SigningKeyService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import murraco.exception.CustomException;
-import murraco.model.Role;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtTokenProvider {
 
+    private static final Logger log = LoggerFactory.getLogger(JwtTokenProvider.class);
     /**
-     * THIS IS NOT A SECURE PRACTICE! For simplicity, we are storing a static key here. Ideally, in a
-     * microservices environment, this key would be kept on a config-server.
+     * 1小时
      */
-    @Value("${security.jwt.token.secret-key:secret-key}")
-    private String secretKey;
-
     @Value("${security.jwt.token.expire-length:3600000}")
     private long validityInMilliseconds = 3600000;
 
     @Autowired
     private MyUserDetails myUserDetails;
 
-    @PostConstruct
-    protected void init() {
-        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
-    }
+    @Autowired
+    private SigningKeyService signingKeyService;
 
     public String createToken(String username, List<Role> roles) {
 
         Claims claims = Jwts.claims().setSubject(username);
-        claims.put("auth", roles.stream().map(s -> new SimpleGrantedAuthority(s.getAuthority())).filter(Objects::nonNull).collect(Collectors.toList()));
+        claims.put("auth", roles.stream().map(s -> new SimpleGrantedAuthority(s.getAuthority())).collect(Collectors.toList()));
 
         Date now = new Date();
         Date validity = new Date(now.getTime() + validityInMilliseconds);
@@ -58,17 +50,26 @@ public class JwtTokenProvider {
                 .setClaims(claims)
                 .setIssuedAt(now)
                 .setExpiration(validity)
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .signWith(SignatureAlgorithm.HS256, signingKeyService.getSigningKey())
                 .compact();
     }
 
-    public Authentication getAuthentication(String token) {
+    Authentication getAuthentication(String token) {
         UserDetails userDetails = myUserDetails.loadUserByUsername(getUsername(token));
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
     public String getUsername(String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+        try {
+            return Jwts.parser().setSigningKey(signingKeyService.getSigningKey()).parseClaimsJws(token).getBody().getSubject();
+        } catch (Exception e) {
+            try {
+                return Jwts.parser().setSigningKey(signingKeyService.getBackupSigningKey()).parseClaimsJws(token).getBody().getSubject();
+            } catch (Exception e1) {
+                log.error("second get user name failed {}", Arrays.toString(e1.getStackTrace()));
+            }
+        }
+        return null;
     }
 
     public String resolveToken(HttpServletRequest req) {
@@ -79,13 +80,19 @@ public class JwtTokenProvider {
         return null;
     }
 
-    public boolean validateToken(String token) {
+    boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            Jwts.parser().setSigningKey(signingKeyService.getSigningKey()).parseClaimsJws(token);
             return true;
-        } catch (JwtException | IllegalArgumentException e) {
-            throw new CustomException("Expired or invalid JWT token", HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (SignatureException e) {
+            log.warn("The token may be signed use back up key");
+            try {
+                Jwts.parser().setSigningKey(signingKeyService.getBackupSigningKey()).parseClaimsJws(token);
+                return true;
+            } catch (Exception e1) {
+                log.error("re validate token failed {}", Arrays.toString(e1.getStackTrace()));
+            }
         }
+        return false;
     }
-
 }
